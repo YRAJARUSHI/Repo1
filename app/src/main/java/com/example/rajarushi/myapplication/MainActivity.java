d6972bf465dc9cfd1702227def9cc41bc89a2e5f
YRAJARUSHI <rajarushiy@outlook.com>
2019-02-27 19:59:15 +1100

    My Application

---
+++ app/src/main/java/com/example/rajarushi/myapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.rajarushi.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void fade(View view)
+    {
+        ImageView pop = (ImageView) findViewById(R.id.popbtn);
+        pop.animate().alpha(0).setDuration(2000);
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
